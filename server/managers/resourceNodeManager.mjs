159081b624e885b909c5ad67361c5497dbbec2b1
Akje <kenrick.ouwehand@gmail.com>
2024-06-17 20:04:01 +0200

    added new nodes and node pngs

--- server/managers/resourceNodeManager.mjs
+++ server/managers/resourceNodeManager.mjs
@@ -1,4 +1,5 @@
 import ResourceNode from "../entities/resourceNode.mjs";
+import serverSettings from "../serverSettings.mjs";
 import GridEntityManager from "./gridEntityManager.mjs";
 
 class ResourceNodeManager extends GridEntityManager {
@@ -12,6 +13,11 @@ class ResourceNodeManager extends GridEntityManager {
         for (let i = 0; i < 48; i++) { this.spawnRandomResourceNode("copperDeposit", 4, 8, 120, 40); }
         for (let i = 0; i < 48; i++) { this.spawnRandomResourceNode("tinDeposit", 4, 8, 120, 40); }
         for (let i = 0; i < 48; i++) { this.spawnRandomResourceNode("ironDeposit", 4, 8, 120, 40); }
+        for (let i = 0; i < 48; i++) { this.spawnRandomResourceNode("zincDeposit", 4, 8, 120, 40); }
+        for (let i = 0; i < 48; i++) { this.spawnRandomResourceNode("leadDeposit", 4, 8, 120, 40); }
+        for (let i = 0; i < 48; i++) { this.spawnRandomResourceNode("silverDeposit", 4, 8, 120, 40); }
+        for (let i = 0; i < 48; i++) { this.spawnRandomResourceNode("coalDeposit", 4, 8, 120, 40); }
+        for (let i = 0; i < 48; i++) { this.spawnRandomResourceNode("saltWater", 0, (serverSettings.mapHeight-1), serverSettings.mapWidth, serverSettings.mapHeight); }
     }
 
     spawnRandomResourceNode(type, xMin, yMin, xMax, yMax) {
